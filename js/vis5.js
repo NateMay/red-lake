b533108cc87d5dc09e6a6d859dc57e60f636f34f
Nathan May <nathaniel.may22@gmail.com>
2021-10-27 12:59:51 -0400

    more style changes

--- js/vis5.js
+++ js/vis5.js
@@ -52,8 +52,6 @@ metrics.forEach((metric) => {
   var svg5 = d3
     .select("#my_dataviz")
     .append("svg")
-    // .attr("width", width + margin.left + margin.right)
-    // .attr("height", height + margin.top + margin.bottom)
     .attr("viewBox", `0 0 ${width + margin.left + margin.right} ${height + margin.top + margin.bottom}`)
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
